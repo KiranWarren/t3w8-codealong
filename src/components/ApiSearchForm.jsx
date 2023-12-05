d6976201b08dbf420d21da9e848107ad55822306
Kiran Warren <kiran.m.warren@gmail.com>
2023-12-05 19:25:18 +1000

    wip pokemon search form

--- src/components/ApiSearchForm.jsx
+++ src/components/ApiSearchForm.jsx
@@ -3,20 +3,27 @@
 // Makes a fetch request on submit
 // Return data
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ApiContext } from "../contexts/ApiProvider";
 
 export function ApiSearchForm() {
   let apiUrlBase = useContext(ApiContext);
+  let [searchData, setSearchData] = useState("");
 
-  const searchForPokemon = () => {
-    console.log(`API URL is: ${apiUrlBase}`);
+  const searchForPokemon = async () => {
+    let response = await fetch(apiUrlBase + "pokemon/" + searchData);
   };
 
   return (
     <div>
       <h5>Enter a Pokemon name:</h5>
-      <input type="text" name="pokemonName" id="pokemonName" />
+      <input
+        type="text"
+        name="pokemonName"
+        id="pokemonName"
+        value={searchData}
+        onChange={(event) => setSearchData(event.target.value)}
+      />
       <button type="submit" onClick={searchForPokemon}>
         Search
       </button>
