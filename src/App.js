9212b7bf650043a179e998bfdb61a4903c090973
Kiran Warren <kiran.m.warren@gmail.com>
2023-12-05 19:05:07 +1000

    fixed babel warning for deployment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import logo from "./logo.svg";
+import { useContext } from "react";
 import "./App.css";
+import { ApiContext } from "./contexts/ApiProvider";
 
 function App() {
-  return <div className="App"></div>;
+  let apiUrl = useContext(ApiContext);
+
+  return (
+    <div className="App">
+      <h1>{apiUrl}</h1>
+    </div>
+  );
 }
 
 export default App;
