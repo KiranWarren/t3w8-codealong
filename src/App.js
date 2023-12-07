30753e3b9e77830b4dfef7a6b29008bfed2764e7
Kiran Warren <kiran.m.warren@gmail.com>
2023-12-07 18:43:06 +1000

    fixed app.js unused variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import { useContext } from "react";
 import "./App.css";
-import { ApiContext } from "./contexts/ApiProvider";
 import { ApiSearchForm } from "./components/ApiSearchForm";
 
 function App() {
-  let apiUrl = useContext(ApiContext);
-
   return (
     <div className="App">
       <ApiSearchForm />
--- src/contexts/ApiProvider.jsx
+++ src/contexts/ApiProvider.jsx
@@ -1,12 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const ApiContext = createContext("https://pokeapi.co/api/v2/");
 
-console.log("process.env.API_URL value is: " + process.env.REACT_APP_API_URL);
-
 export function ApiProvider(props) {
+  let [url, setUrl] = useState(process.env.REACT_APP_API_URL);
+
   return (
-    <ApiContext.Provider value="https://pokeapi.co/api/v2/">
+    <ApiContext.Provider value={{ url, setUrl }}>
       {props.children}
     </ApiContext.Provider>
   );
