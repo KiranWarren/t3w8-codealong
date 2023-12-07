ae69c07e5f9a9492824ccd27bf1bce80b4289a26
Kiran Warren <kiran.m.warren@gmail.com>
2023-12-07 18:28:36 +1000

    added environment file

--- src/contexts/ApiProvider.jsx
+++ src/contexts/ApiProvider.jsx
@@ -2,6 +2,8 @@ import { createContext } from "react";
 
 export const ApiContext = createContext("https://pokeapi.co/api/v2/");
 
+console.log("process.env.API_URL value is: " + process.env.REACT_APP_API_URL);
+
 export function ApiProvider(props) {
   return (
     <ApiContext.Provider value="https://pokeapi.co/api/v2/">
