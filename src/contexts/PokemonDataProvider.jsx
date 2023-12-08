b0a3cbebc57e3958d3504d3a949a521590f714de
Kiran Warren <kiran.m.warren@gmail.com>
2023-12-09 09:43:41 +1000

    team now saving to local storage

--- src/contexts/PokemonDataProvider.jsx
+++ src/contexts/PokemonDataProvider.jsx
@@ -1,16 +1,30 @@
 // Pokemon Data Provider
 // [PokemonObj1, PokemonObj2, PokemonObj3]
 
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { useLocalStorage } from "react-use";
 
 export const PokemonTeamContext = createContext([]);
 
 export function PokemonDataProvider(props) {
-
   let [team, setTeam] = useState([]);
 
+  let [storedTeam, setStoredTeam] = useLocalStorage("pokemonTeamData", team);
+
+  // On app start, load local storage and save it to state
+  useEffect(() => {
+    setTeam(storedTeam);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Whenever team updates, save that to local storage
+  useEffect(() => {
+    setStoredTeam(team);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [team]);
+
   return (
-    <PokemonTeamContext.Provider value={{team, setTeam}}>
+    <PokemonTeamContext.Provider value={{ team, setTeam }}>
       {props.children}
     </PokemonTeamContext.Provider>
   );
