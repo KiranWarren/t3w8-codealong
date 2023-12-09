2ba3559a8741f951b784523e956f3ca6eeba0b2a
Kiran Warren <kiran.m.warren@gmail.com>
2023-12-09 10:02:53 +1000

    pokemon can be deleted from team

--- src/components/PokemonInfoCard.jsx
+++ src/components/PokemonInfoCard.jsx
@@ -1,9 +1,25 @@
 // Display title and image of Pokemon data
 // Data comes from props
 
+import { useContext } from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { PokemonTeamContext } from "../contexts/PokemonDataProvider";
 
 export default function PokemonInfoCard(props) {
+  let { team, setTeam } = useContext(PokemonTeamContext);
+
+  const deletePokemonFromTeam = (providedId) => {
+    // Find and delete pokemon with matching id
+    let teamCopy = JSON.parse(JSON.stringify(team));
+    teamCopy = teamCopy.filter((obj) => {
+      return obj.id !== providedId;
+    });
+
+    // Set the updated team to context
+    setTeam(teamCopy);
+  };
+
   return (
     <Card className="pokemonInfoCard">
       <Card.Body>
@@ -12,6 +28,12 @@ export default function PokemonInfoCard(props) {
           src={props.imageUrl}
           alt="Default front sprite from PokeAPI"
         />
+        <Button
+          onClick={(event) => deletePokemonFromTeam(props.pokemonId)}
+          variant="outline-primary"
+        >
+          Delete
+        </Button>
       </Card.Body>
     </Card>
   );
